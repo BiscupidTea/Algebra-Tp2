7efb62b0eac420917a09d63dac5fe00e01415448
Shusepe <78223164+Shusepe@users.noreply.github.com>
2022-09-28 20:30:27 -0300

    Delete Algebra Tp2 directory

--- Algebra Tp2/camera.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "raylib.h"
-#include <iostream>
-
-using namespace std;
-
-void UserCamera(Vector3 Endline1, Vector3 Endline2, Vector3 Endline3);
--- Algebra Tp2/logic.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#define PI_DEFINES 3.14
-#define CENTIMETER_DEFINES 0.0264583333
-
-using namespace std;
-
-int executeProgram();
